28b76df525c3dde999a311be31f5509f25d74aaa
Dane Reid <danereid@macbookpro.mynetworksettings.com>
2024-11-02 12:09:29 -0400

    new services and mongodb

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private apiUrl = 'http://localhost:3000/api/register';
+
+  constructor(private http: HttpClient) {}
+
+  registerUser(userData: { username: string; email: string; password: string }): Observable<any> {
+    return this.http.post<any>(this.apiUrl, userData);
+  }
+}
